7555727b02d21183e2b6d3d411b19229ec1bd912
BrightMykel <onlineeducation1397@gmail.com>
2023-02-16 20:27:12 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+**
+ * main - Put function to print a line of code
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+    return (0);
+}
