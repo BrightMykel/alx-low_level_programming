f892b8239d0d8364fdff2cf1a82ff53f20e01fd4
BrightMykel <onlineeducation1397@gmail.com>
2023-02-16 21:03:55 -0800

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Printf function to print a line of code
+ *
+ * Return: 0(Success)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
